b68e1751b530dcf171849d8b2b9aee80d3222e03
JeanMartins <85572832+Jean-Martins22@users.noreply.github.com>
2024-12-03 15:08:33 -0300

    feat: Classe Dinâmica

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -1,5 +1,6 @@
 // Components
 import MyComponent from "./components/MyComponent";
+import Title from "./components/Title";
 
 // Syle - Css
 import "./App.css";
@@ -7,7 +8,8 @@ import { useState } from "react";
 
 function App() {
   const n = 15;
-  const [name] = useState("Jean")
+  const [name] = useState("Jean");
+  const redTitle = false;
   return (
     <>
       {/* Css global */}
@@ -26,10 +28,18 @@ function App() {
       <h2 style={n > 10 ? { color: "purple" } : { color: "pink" }}>
         CSS Dinâmico
       </h2>
-      <h2 style={name === "Jean" ? { color: "green", backgroundColor: "#000" } : null}>
+      <h2
+        style={
+          name === "Jean" ? { color: "green", backgroundColor: "#000" } : null
+        }
+      >
         Teste Nome
       </h2>
       {/* Classe dinâmica */}
+      <h2 className={redTitle ? "red-title" : "title"}>
+        Este título vai ter classe dinâmica
+      </h2>
+      <Title />
     </>
   );
 }
