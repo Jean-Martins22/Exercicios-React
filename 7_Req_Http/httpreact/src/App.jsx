14c388c62f665a48807209e602b2d50c14151a2e
JeanMartins <85572832+Jean-Martins22@users.noreply.github.com>
2024-12-03 17:51:08 -0300

    feat: Resgatando dados da API

--- 7_Req_Http/httpreact/src/App.jsx
+++ 7_Req_Http/httpreact/src/App.jsx
@@ -1,14 +1,41 @@
-import './App.css'
+import "./App.css";
+
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // 1 - Resgatando Dados
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
+
 
   return (
     <>
       <div className="App">
         <h1>Lista de Produtos</h1>
+        <ul>
+          {products.map((products) => (
+            <li key={products.id}>
+              {products.name} - R$: {products.price}
+            </li>
+          ))}
+        </ul>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
