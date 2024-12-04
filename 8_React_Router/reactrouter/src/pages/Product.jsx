56c4a84152302cb52323eedd788f2707b11c9524
JeanMartins <85572832+Jean-Martins22@users.noreply.github.com>
2024-12-04 16:47:44 -0300

    feat: Nested Routes

--- 8_React_Router/reactrouter/src/pages/Product.jsx
+++ 8_React_Router/reactrouter/src/pages/Product.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
 
 const Product = () => {
@@ -9,7 +9,7 @@ const Product = () => {
   const url = "http://localhost:3000/products/" + id;
 
   const { data: product, loading, error } = useFetch(url);
-  
+
   return (
     <>
       <p>Id do produto: {id}</p>
@@ -17,8 +17,10 @@ const Product = () => {
       {loading && <p>Carregando...</p>}
       {product && (
         <div>
-            <h1>{product.name}</h1>
-            <p>R$: {product.price}</p>
+          <h1>{product.name}</h1>
+          <p>R$: {product.price}</p>
+          {/* 6 - Nested Routes */}
+          <Link to={`/products/${product.id}/info`}>Mais informações</Link>
         </div>
       )}
     </>
