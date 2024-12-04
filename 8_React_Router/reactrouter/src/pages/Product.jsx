472ac0680556932abdf3b034ca7ea7764bd6aa79
JeanMartins <85572832+Jean-Martins22@users.noreply.github.com>
2024-12-04 16:32:17 -0300

    feat: Carregamento dinâmico de dados

--- 8_React_Router/reactrouter/src/pages/Product.jsx
+++ 8_React_Router/reactrouter/src/pages/Product.jsx
@@ -1,11 +1,26 @@
 import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
 const Product = () => {
   // 4 - Rota Dinâmica
   const { id } = useParams();
+
+  // 5 - Carregamento Dado Individual
+  const url = "http://localhost:3000/products/" + id;
+
+  const { data: product, loading, error } = useFetch(url);
+  
   return (
     <>
       <p>Id do produto: {id}</p>
+      {error && <p>Ocorreu um erro...</p>}
+      {loading && <p>Carregando...</p>}
+      {product && (
+        <div>
+            <h1>{product.name}</h1>
+            <p>R$: {product.price}</p>
+        </div>
+      )}
     </>
   );
 };
