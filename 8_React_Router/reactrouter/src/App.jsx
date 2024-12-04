d905aa7ac3aa7438286be3e89bf7d99e174641d7
JeanMartins <85572832+Jean-Martins22@users.noreply.github.com>
2024-12-04 15:21:20 -0300

    feat: Configurando o React Router

--- 8_React_Router/reactrouter/src/App.jsx
+++ 8_React_Router/reactrouter/src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css'
+// Style - Css
+import "./App.css";
+
+// Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+// 1 - Config React Router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
